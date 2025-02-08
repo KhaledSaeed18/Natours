72d6f51ab615517f3075866171ff127785f04538
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-02-08 20:33:29 +0200

    Add mongoose dependency and configure database connection in server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+dotenv.config({ path: '.env' });
 
 const app = require('./app');
 
 // console.log(app.get('env')); // development
-
 // console.log(process.env); // { ... }
+
+mongoose
+    .connect(process.env.DATABASE_URL)
+    .then(() => console.log('Database Connected Successfully'))
+    .catch(err => console.log(err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
