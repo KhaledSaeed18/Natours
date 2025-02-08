dc8aacf43257d782f0488fa32e4fad8a436a6fee
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-02-08 18:55:55 +0200

    Refactor server initialization by moving it to a new server.js file and updating the dev script in package.json

--- app.js
+++ app.js
@@ -32,8 +32,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// Server Initialization
-const port = 3000;
-app.listen(port, () => {
-    console.log('Server is running on port 3000');
-});
+module.exports = app;
