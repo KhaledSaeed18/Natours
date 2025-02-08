8f4d04b923d20149dabaa21c7e8063a088831425
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-02-08 17:18:35 +0200

    Add GET route for tour by ID

--- app.js
+++ app.js
@@ -18,8 +18,28 @@ app.get('/api/v1/tours', (req, res) => {
     });
 });
 
-app.post('/api/v1/tours', (req, res) => {
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1; // Convert string to number
 
+    // Find the tour with the given id
+    const tour = tours.find(el => el.id === id);
+
+    if (!tour || id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    });
+});
+
+app.post('/api/v1/tours', (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
 
     const newTour = Object.assign({ id: newId }, req.body);
