e550eb66d8f6c32888478129d5009ad1cc23f826
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-02-15 21:29:42 +0200

    Enhance tour model; add new fields for duration, maxGroupSize, difficulty, ratingsQuantity, summary, description, imageCover, images, createdAt, and startDates

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,53 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a name'],
-        unique: true
+        unique: true,
+        trim: true // Remove all white spaces in the beginning and end of the string
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
     },
     price: {
         type: Number,
         required: [true, 'A tour must have a price']
-    }
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a summary']
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image']
+    },
+    images: [String], // Array of Images
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    startDates: [Date] // Array of Dates
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
