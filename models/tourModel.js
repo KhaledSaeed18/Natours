711fd3bc87694402ee57e5cdb2b7e5d5fe58ad28
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-02-16 21:59:21 +0200

    Enhance tour model validation by adding length constraints for tour names and difficulty options, and set rating limits

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,9 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A tour must have a name'],
         unique: true,
-        trim: true // Remove all white spaces in the beginning and end of the string
+        trim: true, // Remove all white spaces in the beginning and end of the string
+        maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+        minlength: [10, 'A tour name must have more or equal than 10 characters']
     },
     slug: String,
     duration: {
@@ -20,10 +22,16 @@ const tourSchema = new mongoose.Schema({
     difficulty: {
         type: String,
         required: [true, 'A tour must have a difficulty'],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'Difficulty is either: easy, medium, difficult'
+        }
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
         type: Number,
@@ -126,9 +134,6 @@ tourSchema.pre('aggregate', function (next) {
     console.log(this.pipeline());
     next();
 });
-
-
-
 
 const Tour = mongoose.model('Tour', tourSchema);
 
