cddc0b59d8b783f7c9ea99ee8c2577949df65191
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-06-30 21:03:54 +0300

    Handle undefined routes

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ if (process.env.NODE_ENV === 'development') {
 // GET /api/v1/tours2 404 2.538 ms - 152
 
 // Middlewares applied to all routes:
-app.use((req, res, next) => {
-    console.log('Hello from the middleware');
-    next(); // Go to the next middleware
-    // ! If next() is not called, the request-response cycle will be stuck.
-});
+// app.use((req, res, next) => {
+//     console.log('Hello from the middleware');
+//     next(); // Go to the next middleware
+//     // ! If next() is not called, the request-response cycle will be stuck.
+// });
 
 app.use((req, res, next) => {
     // Add a new property to the request object
@@ -36,5 +36,16 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Handling undefined routes
+// This middleware will be executed if no other route matches
+// .all method is used to handle all HTTP methods (GET, POST, PUT, DELETE, etc.)
+// '*' means all routes
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server!`
+    });
+});
 
 module.exports = app;
